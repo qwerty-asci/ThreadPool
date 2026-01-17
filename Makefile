2e2d3db190d2f7f6adc65dea232cfd5cdf72bf51
qwerty-asci <pablogacj.trabajo@gmail.com>
2026-01-17 21:05:59 +0100

    Implementation of class ThreadPool without the submit function and everything related with the selection of process

--- Makefile
+++ Makefile
@@ -1,2 +1,16 @@
+all: debug
+
+
+CXX=g++
+CXXFLAGS_PROD=-o main
+CXXFLAGS_DEB=-g -o main -std=c++11
+
+
 main: threadpool.cpp
-	g++ -o main threadpool.cpp
+	@echo "Main version"
+	@${CXX} ${CXXFLAGS_PROD} threadpool.cpp
+
+
+debug: threadpool.cpp
+	@echo "Debuging version"
+	@${CXX} ${CXXFLAGS_DEB} threadpool.cpp
--- threadpool.cpp
+++ threadpool.cpp
@@ -3,13 +3,134 @@
 #include <mutex>
 #include <functional>
 #include <queue>
+#include <exception>
 
 using namespace std;
+
+
+
+class ThreadPool{
+
+private:
+
+	thread * th;
+	mutex mtx;
+	unsigned int num_threads;
+	bool exit_flag,state;
+	int cont=0;
+
+
+
+	//Customized exceptions
+
+	// class exception_thread_allocation:public exception{
+	// 	virtual const char* what() const throw(){
+	// 		return "My exception happened";
+	// 	}
+	// };
+
+public:
+
+	ThreadPool()=delete;
+	ThreadPool(unsigned int num_threads);
+	void submit();
+	void finish(bool secure=true);
+	bool status();
+	~ThreadPool();
+
+};
+
+
+ThreadPool::ThreadPool(unsigned int num_threads):num_threads(num_threads){
+
+
+
+	function<void(void)> f=[&](){
+
+					while(!this->exit_flag && this->state){
+						mtx.lock();
+
+
+						mtx.unlock();
+					}
+				};
+
+
+
+
+
+
+
+	exit_flag=false;
+
+	this->th=new (nothrow) thread[num_threads];
+
+	if(this->th!=nullptr){
+		this->state=true;
+
+		try{
+			for(unsigned int i=0;i<this->num_threads;i++){
+				this->th[i]=thread(f);
+			}
+		}catch(exception &e){
+			this->state=false;
+
+			cout<<"The system is unable to start a new thread"<<endl;
+
+		}
+
+	}else{
+		this->state=false;
+	}
+
+}
+
+bool ThreadPool::status(){
+	return this->state;
+}
+
+
+
+
+void ThreadPool::finish(bool secure){
+	this->exit_flag=true;
+
+	if(secure && this->state){
+		for(unsigned int i=0;i<this->num_threads;i++){
+			if(this->th[i].joinable()){
+				try{
+					this->th[i].join();
+				}catch(...){
+					cout<<"Error joining the thread i"<<endl;
+				}
+			}
+		}
+		this->state=false;
+	}
+}
+
+
+ThreadPool::~ThreadPool(){
+
+	this->finish();
+
+	delete[] this->th;
+}
 
 
 int main(int argc, char * argv[]){
 
 
+	ThreadPool thp{10};
+
+
+	if(thp.status()){
+		thp.finish();
+	}
+
+	if(!thp.status()){
+		cout<<"Cerrado correctamente"<<endl;
+	}
 
 	return 0;
 }
