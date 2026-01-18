d9b822c80d6746627bc9085aa395812df221ae70
qwerty-asci <pablogacj.trabajo@gmail.com>
2026-01-18 11:14:08 +0100

    documentation

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,94 +1,110 @@
 #include "ThreadPool.hpp"
 
+/**
+ * @brief Creates and starts the worker threads.
+ *
+ * Each worker thread continuously checks the task queue and
+ * executes available tasks until the exit flag is set.
+ */
+ThreadPool::ThreadPool(unsigned int num_threads)
+    : num_threads(num_threads) {
 
-ThreadPool::ThreadPool(unsigned int num_threads):num_threads(num_threads){
+    /**
+     * @brief Worker thread function.
+     *
+     * Threads repeatedly fetch tasks from the queue and execute them
+     * while the pool is active and the exit flag is not set.
+     */
+    function<void(void)> f = [&]() {
 
+        function<void()> f;
 
+        while (!this->exit_flag && this->state) {
+            mtx.lock();
 
-	function<void(void)> f=[&](){
+            if (!this->q.empty()) {
+                f = this->q.front();
+                q.pop();
 
-					function<void()> f;
+                mtx.unlock();
 
-					while(!this->exit_flag && this->state){
-						mtx.lock();
+                f();
+            } else {
+                mtx.unlock();
+            }
+        }
+    };
 
-						if(!this->q.empty()){
-							f=this->q.front();
-							q.pop();
+    exit_flag = false;
 
-							mtx.unlock();
+    // Allocate thread array
+    this->th = new (nothrow) thread[num_threads];
 
-							f();
-						}else{
-							mtx.unlock();
-						}
-					}
-				};
+    if (this->th != nullptr) {
+        this->state = true;
 
-	exit_flag=false;
+        try {
+            for (unsigned int i = 0; i < this->num_threads; i++) {
+                this->th[i] = thread(f);
+            }
+        } catch (exception& e) {
+            this->state = false;
+            cout << "The system is unable to start a new thread" << endl;
+        }
 
-	this->th=new (nothrow) thread[num_threads];
-
-	if(this->th!=nullptr){
-		this->state=true;
-
-		try{
-			for(unsigned int i=0;i<this->num_threads;i++){
-				this->th[i]=thread(f);
-			}
-		}catch(exception &e){
-			this->state=false;
-
-			cout<<"The system is unable to start a new thread"<<endl;
-
-		}
-
-	}else{
-		this->state=false;
-	}
-
+    } else {
+        this->state = false;
+    }
 }
 
-bool ThreadPool::status(){
-	return this->state;
+/**
+ * @brief Returns whether the thread pool is running.
+ */
+bool ThreadPool::status() {
+    return this->state;
 }
 
+/**
+ * @brief Signals all threads to stop and optionally joins them.
+ *
+ * If secure mode is enabled, this function waits for all
+ * worker threads to terminate before returning.
+ */
+void ThreadPool::finish(bool secure) {
+    this->exit_flag = true;
 
-
-
-
-void ThreadPool::finish(bool secure){
-	this->exit_flag=true;
-
-	if(secure && this->state){
-		for(unsigned int i=0;i<this->num_threads;i++){
-			if(this->th[i].joinable()){
-				try{
-					this->th[i].join();
-				}catch(...){
-					cout<<"Error joining the thread i"<<endl;
-				}
-			}
-		}
-		this->state=false;
-	}
+    if (secure && this->state) {
+        for (unsigned int i = 0; i < this->num_threads; i++) {
+            if (this->th[i].joinable()) {
+                try {
+                    this->th[i].join();
+                } catch (...) {
+                    cout << "Error joining the thread i" << endl;
+                }
+            }
+        }
+        this->state = false;
+    }
 }
 
+/**
+ * @brief Destructor.
+ *
+ * Ensures that all threads are stopped and memory is released.
+ */
+ThreadPool::~ThreadPool() {
 
-ThreadPool::~ThreadPool(){
-
-	this->finish();
-
-	delete[] this->th;
+    this->finish();
+    delete[] this->th;
 }
 
-
-
-void ThreadPool::wait(){
-	while(!this->q.empty()){
-
-	}
+/**
+ * @brief Waits until the task queue becomes empty.
+ *
+ * This method performs a busy wait and does not block efficiently.
+ */
+void ThreadPool::wait() {
+    while (!this->q.empty()) {
+        // Busy wait
+    }
 }
-
-
-
--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -8,56 +8,124 @@
 #include <queue>
 #include <exception>
 
-
 using namespace std;
 
-
-
-class ThreadPool{
+/**
+ * @class ThreadPool
+ * @brief Simple thread pool implementation for executing tasks concurrently.
+ *
+ * This class manages a fixed number of worker threads that continuously
+ * execute tasks stored in a shared queue. Tasks are submitted as callable
+ * objects with no return value.
+ */
+class ThreadPool {
 
 private:
 
-	thread * th;
-	mutex mtx;
-	queue<function<void()>> q;
-	unsigned int num_threads;
-	bool exit_flag,state;
-	int cont=0;
+    /**
+     * @brief Pointer to the array of worker threads.
+     */
+    thread* th;
 
+    /**
+     * @brief Mutex used to protect access to the task queue.
+     */
+    mutex mtx;
 
+    /**
+     * @brief Queue that stores the tasks to be executed by the threads.
+     */
+    queue<function<void()>> q;
+
+    /**
+     * @brief Number of worker threads in the pool.
+     */
+    unsigned int num_threads;
+
+    /**
+     * @brief Flag indicating when threads should stop execution.
+     */
+    bool exit_flag;
+
+    /**
+     * @brief Indicates whether the thread pool is running correctly.
+     */
+    bool state;
+
+    /**
+     * @brief Auxiliary counter (currently unused).
+     */
+    int cont = 0;
 
 public:
 
-	ThreadPool()=delete;
-	ThreadPool(unsigned int num_threads);
+    /**
+     * @brief Default constructor deleted to force explicit thread count.
+     */
+    ThreadPool() = delete;
 
-	template <class Func,class... Args>
-	void submit(Func&& f,Args&&... args);
-	void finish(bool secure=true);
-	bool status();
-	void wait();
-	~ThreadPool();
+    /**
+     * @brief Constructs a ThreadPool with a fixed number of threads.
+     * @param num_threads Number of worker threads to create.
+     */
+    ThreadPool(unsigned int num_threads);
 
+    /**
+     * @brief Submits a task to the thread pool.
+     *
+     * The task is stored in the internal queue and will be executed
+     * by one of the worker threads.
+     *
+     * @tparam Func Callable type.
+     * @tparam Args Argument types for the callable.
+     * @param f Function to execute.
+     * @param args Arguments passed to the function.
+     */
+    template <class Func, class... Args>
+    void submit(Func&& f, Args&&... args);
 
+    /**
+     * @brief Stops the thread pool and optionally joins all threads.
+     *
+     * @param secure If true, waits for all threads to finish execution.
+     */
+    void finish(bool secure = true);
+
+    /**
+     * @brief Returns the current state of the thread pool.
+     * @return True if the pool is active, false otherwise.
+     */
+    bool status();
+
+    /**
+     * @brief Busy-waits until the task queue becomes empty.
+     */
+    void wait();
+
+    /**
+     * @brief Destructor. Stops the pool and releases resources.
+     */
+    ~ThreadPool();
 };
 
+/**
+ * @brief Adds a new task to the task queue.
+ *
+ * This method locks the queue mutex, pushes a new task,
+ * and then unlocks the mutex.
+ */
+template <class Func, class... Args>
+void ThreadPool::submit(Func&& f, Args&&... args) {
 
+    mtx.lock();
 
+    this->q.push(
+        [&]() {
+            f(args...);
+        }
+    );
 
-
-template <class Func,class... Args>
-void ThreadPool::submit(Func&& f,Args&&... args){
-
-	mtx.lock();
-
-	this->q.push(
-		[&](){
-			f(args...);
-		}
-	);
-
-	mtx.unlock();
-
+    mtx.unlock();
 }
 
 #endif
